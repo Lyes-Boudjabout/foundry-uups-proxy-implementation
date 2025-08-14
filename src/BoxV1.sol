e5ff5ea9c029fb90655ab31e1cc77a6aa6a21e6b
Lyes Boudjabout <nl_boudjabout@esi.dz>
2025-08-14 09:38:57 +0100

    implementing the upgrade functionality

---
+++ src/BoxV1.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import {UUPSUpgradeable} from "@openzeppelin-upgrades/contracts/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin-upgrades/contracts/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin-upgrades/contracts/access/OwnableUpgradeable.sol";
+
+contract BoxV1 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+    uint256 internal number;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() public initializer {
+        __Ownable_init(msg.sender);
+        __UUPSUpgradeable_init();
+    }
+
+    function getNumber() external view returns(uint256) {
+        return number;
+    }
+
+    function version() external pure returns(uint256) {
+        return 1;
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+}
---
+++ src/BoxV2.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import {UUPSUpgradeable} from "@openzeppelin-upgrades/contracts/proxy/utils/UUPSUpgradeable.sol";
+
+contract BoxV2 is UUPSUpgradeable {
+    uint256 internal number;
+
+    function setNumber(uint256 _number) public {
+        number = _number;
+    }
+
+    function getNumber() external view returns(uint256) {
+        return number;
+    }
+
+    function version() external pure returns(uint256) {
+        return 2;
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal view override {}
+}
